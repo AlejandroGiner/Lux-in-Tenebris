b5e978b076682efec2ca3e66a0775520d0b8ff77
Narediel <mutilador_1996@hotmail.com>
2019-04-12 05:21:27 +0200

    defined aaron

--- common/defines/btp_hre.lua
+++ common/defines/btp_hre.lua
@@ -2,10 +2,12 @@ NDefines.NDiplomacy.MAX_FREE_CITIES = 15			-- from original BtP increased from 8
 NDefines.NDiplomacy.HRE_PRINCE_AUTHORITY_THRESHOLD = 35			-- from original BtP increased from 25
 NDefines.NDiplomacy.IMPERIAL_AUTHORITY_FROM_PRINCES = 0.075			-- from original BtP reduced from 01
 
-
+NDefines.NDiplomacy.NUM_OF_GREAT_POWERS = 10
 NDefines.NDiplomacy.IMPERIAL_CITY_IA = 0.003			-- Beyond Typus added reducing ia to account for all the new provinces Base 0005
 NDefines.NDiplomacy.HRE_FOREIGN_CONTROL_PENALTY = -0.003			-- Beyond Typus added reducing ia to account for all the new provinces Base 0005
-NDefines.NCountry.MERCHANT_REPUBLIC_SIZE_LIMIT = 40			-- Beyond Typus added large number of new provinces make province number based things like this require updating base 20
-NDefines.NCountry.PS_BOOST_MILITARIZATION = 30			-- Same logic as above new provinces cause faster decline thus need lower cost ideally I can find a better solution later Base 50
-NDefines.NMilitary.FORT_PER_DEV_RATIO = 30			-- Beyond Typus added more provinces means more forts needed for same dev Base 50
+NDefines.NCountry.MERCHANT_REPUBLIC_SIZE_LIMIT = 55			-- Beyond Typus added large number of new provinces make province number based things like this require updating base 20
+NDefines.NCountry.PS_BOOST_MILITARIZATION = 25			-- Same logic as above new provinces cause faster decline thus need lower cost ideally I can find a better solution later Base 50
+NDefines.NMilitary.FORT_PER_DEV_RATIO = 25			-- Beyond Typus added more provinces means more forts needed for same dev Base 50
 NDefines.NMilitary.FORTRESS_COST = 0.25			-- Beyond Typus added all the new provinces mean more forts but no more dev so everyone was going broke Base 05
+NDefines.NCountry.ESTATE_PROVINCE_HAPPINESS_DECREASE = 3.5
+NDefines.NCountry.MIN_DEV_FOR_FREE_CITY = 8
