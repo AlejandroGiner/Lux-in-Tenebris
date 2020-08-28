f1cabb3904351746bedb919b31d764d7abbc1919
Slaydown Menethil <mutilador_1996@hotmail.com>
2020-08-28 07:47:03 +0200

    Korea+Nippon node changes, tweaks to trade value modifier

---
+++ common/defines/btc_defines.lua
@@ -0,0 +1,26 @@
+NDefines.NDiplomacy.MAX_FREE_CITIES = 20			-- from original BtP increased from 8
+NDefines.NDiplomacy.HRE_PRINCE_AUTHORITY_THRESHOLD = 45			-- from original BtP increased from 25
+NDefines.NDiplomacy.IMPERIAL_AUTHORITY_FROM_PRINCES = 0.075			-- from original BtP reduced from 01
+
+NDefines.NDiplomacy.NUM_OF_GREAT_POWERS = 10
+NDefines.NDiplomacy.IMPERIAL_CITY_IA = 0.003			-- Beyond Typus added reducing ia to account for all the new provinces Base 0005
+NDefines.NDiplomacy.HRE_FOREIGN_CONTROL_PENALTY = -0.003			-- Beyond Typus added reducing ia to account for all the new provinces Base 0005
+
+NDefines.NMilitary.FORT_PER_DEV_RATIO = 30			-- Beyond Typus added more provinces means more forts needed for same dev Base 50
+NDefines.NMilitary.FORTRESS_COST = 0.35			-- Beyond Typus added all the new provinces mean more forts but no more dev so everyone was going broke Base 05
+
+NDefines.NCountry.MIN_DEV_FOR_FREE_CITY = 8
+
+NDefines.NEconomy.CARAVAN_POWER_MAX = 35
+NDefines.NEconomy.CARAVAN_FACTOR = 6.0,							-- More development in LiT in general 210 development needed to reach max 35
+NDefines.NEconomy.TRADE_ADDED_VALUE_MODIFER = 0.02			-- from 0 5 many more nodes in LiT
+
+NDefines.NDiplomacy.PO_DEMAND_PROVINCES_AE = 0.65			-- from 0 75
+NDefines.NDiplomacy.PO_DEMAND_PROVINCES_PRESTIGE = 0.2			-- from 0 25
+NDefines.NDiplomacy.PEACE_COST_DEMAND_PROVINCE = 0.8			-- from 1
+NDefines.NDiplomacy.PEACE_COST_CONCEDE_PROVINCE = 0.8			-- from 1
+NDefines.NDiplomacy.PEACE_COST_RETURN_CORE = 0.8			-- from 1
+NDefines.NDiplomacy.PEACE_COST_BECOME_VASSAL = 0.8			-- from 1
+NDefines.NDiplomacy.PEACE_COST_REVOKE_CORE = 0.4			-- from 0 5
+
+NDefines.NAI.TRADE_INTEREST_THRESHOLD = 4			-- Vanilla is 3 LiT increased base merchants to 3 so increasing this as well by 1
